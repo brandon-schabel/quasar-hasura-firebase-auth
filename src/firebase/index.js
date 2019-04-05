ce8ed1e2f62f40f920c2f822abcbaf11377356cc
brandon-schabel <brandonschabel1995@gmail.com>
2019-04-04 19:01:08 -0700

    add prettier.rc and firebase config

---
+++ src/firebase/index.js
@@ -0,0 +1,14 @@
+import firebase, { initializeApp } from 'firebase'
+
+const config = {
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN
+}
+
+export const app = initializeApp(config)
+export const db = app.firestore()
+export const namesRef = db.collection('names')
+export const auth = app.auth()
+export const googleSignInProvider = new firebase.auth.GoogleAuthProvider()
